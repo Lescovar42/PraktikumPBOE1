2bfc3158b6e653ff62a7c732b9e68fb001e6dbd6
Lescovar42 <mfarhanazis@gmail.com>
2026-06-18 18:26:12 +0700

    Add Pertemuan 11

---
+++ Pertemuan11/src/main/java/com/mycompany/pbo11a_24060124140166/PBO11A_24060124140166.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo11a_24060124140166;
+
+/**
+ *
+ * @author Muhammad Farhan Abdul Azis
+ */
+public class PBO11A_24060124140166 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
