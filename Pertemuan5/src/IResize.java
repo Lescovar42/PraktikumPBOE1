fc882c37b64aae6aa5165c5571e5b056099835b5
Lescovar42 <mfarhanazis@gmail.com>
2026-06-18 18:26:03 +0700

    Add Pertemuan 5

---
+++ Pertemuan5/src/IResize.java
@@ -0,0 +1,13 @@
+/* Nama File    : IResize.java
+ * Deskripsi    : Class interface untuk melakukan resize
+ * Pembuat      : Muhammad Farhan Abdul Azis
+ * Tanggal      : 18 Maret 2026
+ */
+
+public interface IResize {
+    public void zoomIn();
+
+    public void zoomOut();
+
+    public void zoom(int percent);
+}
